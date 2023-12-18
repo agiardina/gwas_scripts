4979428cc97c7d860207b7961cf1a82c98fe0f1a
Andrea Giardina <andrea.giardina1@open.ac.uk>
2023-12-18 15:04:22 +0100

    Script to add average column to csv file added

---
+++ src/addavgcol.R
@@ -0,0 +1,22 @@
+library("optparse")
+library("ggplot2")
+library("ggforce")
+library("concaveman")
+
+
+option_list = list(
+  make_option(c("-i","--input"), type="character", default=NULL,
+              help="Input file", metavar="character"),    
+  make_option(c("--cols"), type="character", default=NULL,
+              help="Comma separated columns to calculate the average", metavar="character"),
+  make_option(c("--name"), type="character", default=NULL, 
+              help="Name of the new column to add", metavar="character")
+)
+
+opt_parser <- OptionParser(option_list=option_list);
+opt <- parse_args(opt_parser);
+
+cols <- unlist(strsplit(opt$cols, ","))
+df <- read.csv(opt$input)
+df[,opt$name] <- (df[,cols[1]] + df[,cols[2]])/2
+write.csv(df,opt$input,row.names=FALSE)
