74120f8fa9e11265e6cc0f3f6d61deb884191576
Andrea Giardina <andrea.giardina1@open.ac.uk>
2023-12-20 09:36:25 +0100

    Fix for MAF distribution plot

--- src/MAF_check.R
+++ src/MAF_check.R
@@ -1,5 +1,5 @@
 working_dir<-commandArgs(trailingOnly=TRUE)[1]
 maf_freq <- read.table(paste(working_dir,"/MAF_check.frq",sep=""), header =TRUE, as.is=T)
-pdf(paste(working_dir,"MAF_distribution.pdf",sep=""))
+pdf(paste(working_dir,"/MAF_distribution.pdf",sep=""))
 hist(maf_freq[,5],main = "MAF distribution", xlab = "MAF")
 dev.off()
