722bb1d6cbe8247656fe9af6118780baa65f9ee0
Andrea Giardina <andrea.giardina1@open.ac.uk>
2024-01-18 19:48:08 +0100

    Script to generate Manhattan for R2 added

---
+++ src/R2manhattan.R
@@ -0,0 +1,17 @@
+library("fastman")
+library("Cairo")
+
+option_list = list(
+  make_option(c("-i","--input"), type="character", default=NULL,
+              help="Input file", metavar="character"),    
+  make_option(c("-o", "--out"), type="character", default=NULL,
+              help="Path of the png file", metavar="character"),
+)
+opt_parser <- OptionParser(option_list=option_list);
+opt <- parse_args(opt_parser);
+
+df = read.table(opt$input,header=TRUE)
+
+CairoPNG(opt$out, width=10, height=6, units="in", res=300)
+fastman(df, p="R2", maxP=NULL, logp = FALSE, suggestiveline = 0.005, genomewideline = 0.01, annotatePval=1E-5, geneannotate = TRUE, build = 37)
+dev.off()
